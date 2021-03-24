92b9e55544c189ede6ee0b1cfaa7f41dcc378a2d
mzh <70004810+halomzh@users.noreply.github.com>
2021-03-24 22:45:57 +0800

    修改使用案例

--- halo-cache-example/src/main/java/com.halo.cache.example/ExampleApp.java
+++ halo-cache-example/src/main/java/com.halo.cache.example/ExampleApp.java
@@ -1,6 +1,8 @@
 package com.halo.cache.example;
 
+import com.halo.cache.annotation.CacheEvict;
 import com.halo.cache.annotation.CacheGet;
+import com.halo.cache.annotation.CachePut;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,10 +25,29 @@ public class ExampleApp {
 	}
 
 	@GetMapping("/get")
-	@CacheGet(name = "#id", nameSpace = "app")
-	public String get(@RequestParam(name = "id") String id) {
+	@CacheGet(nameSpace = "example", name = "'id:' + #id", condition = "#id != '5'")
+	public String get(@RequestParam(name = "id") String id, @RequestParam(name = "name") String name) {
 		log.info("进入get程序");
-		return "12312312312312312#" + id;
+		return name + "#12312312312312312#" + id;
+	}
+
+	@GetMapping("/put")
+	@CachePut(nameSpace = "example", name = "'id:' + #id", condition = "#id != '5'")
+	public String put(@RequestParam(name = "id") String id, @RequestParam(name = "name") String name) {
+		log.info("进入put程序");
+		return name + "#12312312312312312#" + id;
+	}
+
+	@GetMapping("/evict")
+	@CacheEvict(nameSpace = "example", names = "'id:' + #id")
+	public void evict(@RequestParam(name = "id") String id) {
+		log.info("删除缓存: id[{}]", id);
+	}
+
+	@GetMapping("/evictAll")
+	@CacheEvict(nameSpace = "example", allEntries = true, condition = "#del")
+	public void evictAll(@RequestParam(name = "del") boolean del) {
+		log.info("删除所有缓存");
 	}
 
 }
